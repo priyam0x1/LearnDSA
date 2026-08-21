c2b9a9004ce018265e040700e817f64581265c52
Priyam <priyampratim57@gmail.com>
2026-08-21 13:21:50 +0530

    Learning about operator

---
+++ DAY-02/operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Arithmetic Operator
+    int a = 5;
+    int b = 3;
+
+    cout<<"+ "<<(a+b)<<endl; //8
+    cout<<"- "<<(a-b)<<endl; //2
+    cout<<"* "<<(a*b)<<endl; //15
+    cout<<"/ "<<(a/b)<<endl; //1
+    cout<<"% "<<(a%b)<<endl; //2
+
+    return 0;
+}
---
+++ DAY-02/unary.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    a++;
+    cout<<"a++ "<<a<<endl; //4
+    a--;
+    cout<<"a-- "<<a<<endl; //3
+    return 0;
+}
