c7d37053dc7beb8ce729a262d6f8c6611765f806
Priyam <priyampratim57@gmail.com>
2026-08-21 10:43:54 +0530

    Sum of two digit

---
+++ DAY-01/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"Enter The Value Of A : ";
+    cin>>a;
+    cout<<"Enter The Value Of B : ";
+    cin>>b;
+
+    cout<<"Sum of A + B : "<<a+b<<endl;
+    return 0;
+}
