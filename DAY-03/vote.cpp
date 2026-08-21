ba4787b7e1f8de2d1d6d9e0e65b44126267428ef
Priyam <priyampratim57@gmail.com>
2026-08-22 00:00:48 +0530

    If-else condition

---
+++ DAY-03/vote.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 14;
+    if(age>=18){
+        cout<<"Person Can Vote";
+    }else{
+        cout<<"Person can not vote";
+    }
+    return 0;
+}
