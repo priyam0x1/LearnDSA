eda7ef0d58bbd12e7571a2c22dd46f14c9d3d71b
Priyam <priyampratim57@gmail.com>
2026-08-21 12:51:05 +0530

    Learn type conversion

---
+++ DAY-02/convert.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// For typecasting the order is : 
+// bool -> char -> int -> float -> double
+    cout<<(23.5 + 2 + 'A'); // Output = 90.5
+    return 0;
+}
