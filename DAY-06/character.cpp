7a60b9ef5158c82b9dde5b229039e4d09475b3b1
Priyam <priyampratim57@gmail.com>
2026-08-30 23:59:33 +0530

    next character

---
+++ DAY-06/character.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void ch(char ch);
+int main()
+{
+    ch('a');
+    return 0;
+}
+
+void ch(char ch)
+{
+    if (ch == 'Z')
+    {
+        cout << 'A';
+    }
+    else if (ch == 'z')
+    {
+        cout << 'a';
+    }
+    else
+    {
+        char next = ch + 1;
+        cout << next;
+    }
+}
