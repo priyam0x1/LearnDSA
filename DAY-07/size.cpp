102542d1e0f2b5c449106c3683f41dcf5dd4a3ba
Priyam <priyampratim57@gmail.com>
2026-08-31 22:44:28 +0530

    Data type size and modifiers

---
+++ DAY-07/size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << sizeof(int) << " \n";
+    cout << sizeof(long int) << " \n";
+
+    cout << sizeof(double) << " \n";
+    cout << sizeof(long double) << " \n";
+    return 0;
+}
