ab59f2b136e0c8bc9524f9e52c12ed1b9cc2c1a2
Priyam <priyampratim57@gmail.com>
2026-09-02 21:49:17 +0530

    Array Input using loop

---
+++ Array/Array-01/arrInput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    int n = sizeof(arr) / sizeof(int);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
