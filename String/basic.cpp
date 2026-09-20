c703d4825092825c1423bbe0fa9a4d3d51a441a7
Priyam <priyampratim57@gmail.com>
2026-09-20 23:57:27 +0530

    Basic Strings

---
+++ String/basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    // char work[] = "code";
+    // cout << work << "\n";
+    // cout << sizeof(work) << "\n";
+    // cout << strlen(work) << "\n";
+    char work[30];
+    cin >> work;
+
+    cout << "Your word is : " << work;
+    return 0;
+}
