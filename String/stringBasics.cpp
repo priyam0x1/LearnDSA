44d1aa72d9939dab0deedea65b52d83be8b41d2c
Priyam <priyampratim57@gmail.com>
2026-09-23 17:25:18 +0530

    String basics

---
+++ String/stringBasics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // string str = "hello";
+    // cout << str << endl;
+    // str = "hiiiii";
+    // cout << str << endl;
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+}
